39e13882380a7c33ee653a18e27f8462cf7a304f
Daniel Wedin <danielwedin@hotmail.se>
2019-08-14 14:18:47 +0200

    Filter and pagination, and createdAt for links

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,27 @@
-function feed(parent, args, context, info) {
-  return context.prisma.links();
+async function feed(parent, args, context, info) {
+  const filter = args.filter
+    ? {
+        OR: [{ description_contains: args.filter }, { url_contains: args.filter }],
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where: filter,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma
+    .linksConnection({
+      where: filter,
+    })
+    .aggregate()
+    .count();
+  return {
+    links,
+    count,
+  };
 }
 
 module.exports = {
