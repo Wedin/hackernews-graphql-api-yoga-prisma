90aa9a241d21e039033df7f8c62e2db00afc1d8b
Daniel Wedin <danielwedin@hotmail.se>
2019-08-14 14:37:05 +0200

    Move creation of graphql server

--- src/createServer.js
+++ src/createServer.js
@@ -1,1 +1,34 @@
-//TODO MOVE THIS HERE
+const { GraphQLServer } = require("graphql-yoga");
+require("./config");
+// Read config before creating prisma client
+const { prisma } = require("./generated/prisma-client");
+const Subscription = require("./resolvers/Subscription");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Vote = require("./resolvers/Vote");
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote,
+};
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: "./src/schema.graphql",
+    resolvers,
+    context: request => {
+      return {
+        ...request,
+        prisma,
+      };
+    },
+  });
+}
+
+module.exports = createServer;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
-const { GraphQLServer } = require("graphql-yoga");
-require("./config"); // Read config before creating prisma client
-const { prisma } = require("./generated/prisma-client");
-const Subscription = require("./resolvers/Subscription");
-const Query = require("./resolvers/Query");
-const Mutation = require("./resolvers/Mutation");
-const User = require("./resolvers/User");
-const Link = require("./resolvers/Link");
-const Vote = require("./resolvers/Vote");
+const createServer = require("./createServer");
+const { PORT } = require("./config");
+const port = PORT || 4000;
 
-const resolvers = {
-  Query,
-  Mutation,
-  Subscription,
-  User,
-  Link,
-  Vote,
-};
-
-const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers,
-  context: request => {
-    return {
-      ...request,
-      prisma,
-    };
-  },
-});
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+const server = createServer();
+server.start({ port: port }, () => console.log(`Server is running on http://localhost:${port}`));
